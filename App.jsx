41c5994f64bd9a7e8a6040ef1bfd9ec899d47ac1
Ravespapito <piewtfboom@gmail.com>
2025-06-24 16:37:07 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to RaveOracle 🌈</h1>
+      <p>This is your AI rave outfit assistant!</p>
+    </div>
+  )
+}
